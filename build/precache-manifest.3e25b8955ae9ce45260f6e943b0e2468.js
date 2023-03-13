6eab37950dff3a9f61f7abf98a6918a4215b29cf
bits-and-bytes85 <57238679+bits-and-bytes85@users.noreply.github.com>
2023-03-13 15:19:31 -0700

    Add files via upload

---
+++ build/precache-manifest.3e25b8955ae9ce45260f6e943b0e2468.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af83f8d34f5d071e223d868ea52682e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be0109e7b30481b0a657",
+    "url": "/static/js/2.1ee9eee7.chunk.js"
+  },
+  {
+    "revision": "aa6820f47373d75605b620ed324b9fef",
+    "url": "/static/js/2.1ee9eee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940f3046d5c04328c0c0",
+    "url": "/static/js/main.868bf0a4.chunk.js"
+  },
+  {
+    "revision": "c7805d0a662790c235f8",
+    "url": "/static/js/runtime-main.c0105f97.js"
+  },
+  {
+    "revision": "c19bcc64e0e020cf30060011cb5c4242",
+    "url": "/static/media/background.c19bcc64.jpeg"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "/static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "f14b1d2f421e14b93efb183d0cbf768b",
+    "url": "/static/media/logo.f14b1d2f.svg"
+  },
+  {
+    "revision": "0e48903e734324d3fa4ae30ce51535dd",
+    "url": "/static/media/logo_white.0e48903e.svg"
+  },
+  {
+    "revision": "56abb13122b63b534b2e487e6f16f29d",
+    "url": "/static/media/openexa_logonew.56abb131.png"
+  }
+]);
